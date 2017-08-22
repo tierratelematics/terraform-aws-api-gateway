db325651c1386128e374555630e32dfda42ee7a0
Maurizio Branca <mbranca@tierratelematics.com>
2017-08-22 18:45:51 +0200

    Initial import

---
+++ modules/api-gateway/integration-lambda/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_version = ">= 0.9, < 0.10"
+}
+
+/*
+ * Resources
+ */
+
+//
+// Request phase
+//
+
+resource "aws_api_gateway_method" "request_method" {
+  rest_api_id   = "${var.api_gateway_id}"
+  resource_id   = "${var.api_gateway_resource_id}"
+  http_method   = "${var.http_method}"
+  authorization = "${var.authorization}"
+}
+
+resource "aws_api_gateway_integration" "request_integration" {
+  rest_api_id             = "${var.api_gateway_id}"
+  resource_id             = "${var.api_gateway_resource_id}"
+  http_method             = "${aws_api_gateway_method.request_method.http_method}"
+  integration_http_method = "${var.integration_http_method}"
+  type                    = "${var.integration_type}"
+  uri                     = "${var.api_gateway_lambda_arn}"
+  credentials             = "${var.api_gateway_role_arn}"
+
+  content_handling     = "${var.content_handling}"
+  passthrough_behavior = "${var.passthrough_behavior}"
+
+  request_templates    = "${var.integration_request_templates}"
+}
+
+//
+// Response phase
+//
+
+resource "aws_api_gateway_integration_response" "response_integration" {
+  rest_api_id = "${var.api_gateway_id}"
+  resource_id = "${var.api_gateway_resource_id}"
+  http_method = "${aws_api_gateway_method.request_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.response_method.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  response_templates = "${var.integration_response_templates}"
+}
+
+resource "aws_api_gateway_method_response" "response_method" {
+  rest_api_id = "${var.api_gateway_id}"
+  resource_id = "${var.api_gateway_resource_id}"
+  http_method = "${aws_api_gateway_method.request_method.http_method}"
+  status_code = "200"
+
+  response_models = "${var.method_response_models}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
---
+++ modules/api-gateway/integration-lambda/vars.tf
@@ -0,0 +1,77 @@
+/*
+ * Required
+ */
+
+variable "api_gateway_id" {
+  description = "The ID of the REST API."
+}
+
+variable "api_gateway_resource_id" {
+  description = "The API resource ID where attach the Lambda integration."
+}
+
+variable "api_gateway_role_arn" {
+  description = "The ARN of the IAM Role used by the API Gateway"
+}
+
+variable "api_gateway_lambda_arn" {
+  description = "The ARN of the Lambda for the integration"
+}
+
+/*
+ * Optional
+ */
+
+variable "http_method" {
+  default = "POST"
+}
+
+variable "integration_http_method" {
+  default = "POST"
+}
+
+variable "integration_type" {
+  default = "AWS"
+}
+
+variable "authorization" {
+  description = "The type of authorization used for the method (NONE, CUSTOM, AWS_IAM)"
+  default     = "NONE"
+}
+
+variable "method_request_models" {
+  type = "map"
+
+  default = {}
+}
+
+variable "content_handling" {
+  description = "Specifies how to handle request payload content type conversions. Supported values are CONVERT_TO_BINARY and CONVERT_TO_TEXT. If this property is not defined, the request payload will be passed through from the method request to integration request without modification, provided that the passthroughBehaviors is configured to support payload pass-through."
+  default = ""
+}
+
+variable "passthrough_behavior" {
+  description = "The integration passthrough behavior (WHEN_NO_MATCH, WHEN_NO_TEMPLATES, NEVER). Required if request_templates is used."
+  default     = "NEVER"
+}
+
+variable "integration_request_templates" {
+  description = "A map of the integration's request templates."
+  type        = "map"
+
+  default = {
+    "application/json" = ""
+  }
+}
+
+variable "integration_response_templates" {
+  type = "map"
+
+  default = {}
+}
+
+variable "method_response_models" {
+  type = "map"
+
+  default = {}
+}
